b64757a8c6a4d80057e9ba80b5cbaedcfe25ede1
Alexey <>
2024-06-27 15:39:39 +0300

    Добавьте файлы проекта.

---
+++ client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+
+        using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+            Console.WriteLine("Connecting");
+
+            client.Connect(remoteEndPoint);
+
+            Console.WriteLine("Connected");
+
+            byte[] bytes = Encoding.UTF8.GetBytes("Hello");
+
+            int count = client.Send(bytes);
+
+            if (count == bytes.Length)
+            {
+                Console.WriteLine("Отправленно");
+            }
+            else
+            {
+                Console.WriteLine("Something wrong");
+            }
+
+        }
+    }
+}
+
---
+++ sockets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+            listener.Blocking = true;
+
+            listener.Bind(localEndPoint);
+
+            listener.Listen(100);
+
+            Console.WriteLine("Waiting connect");
+
+            var socket = listener.Accept();
+
+            Console.WriteLine("Connected");
+
+            byte[] buffer = new byte[255];
+            int count = socket.Receive(buffer);
+
+            if (count > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer);
+
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Сообщение не получено");
+            }
+
+            listener.Close();
+        }
+    }
+}
